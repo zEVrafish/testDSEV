fdfc94164d4670998da48180421a16ed6f8568dd
Anders Askeland <anders.askeland@gmail.com>
2021-11-06 17:25:18 +0100

    Added configs

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: ["./src/**/*.html", "./src/**/*.njk", "./src/*.njk"],
+    darkMode: "class", // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                background: "#232325",
+            },
+            fontFamily: {
+                montserrat: ["Montserrat", '"-apple-system"', "BlinkMacSystemFont"],
+            },
+        },
+    },
+    variants: {
+        extend: {
+            display: ["dark"],
+        },
+    },
+    plugins: [],
+};
