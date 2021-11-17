33d352c5a52c3e3b378a86182e7f51d0901abe09
Anders Askeland <anders.askeland@gmail.com>
2021-11-17 14:56:22 +0100

    Mobile design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
     darkMode: "class", // or 'media' or 'class'
     theme: {
         extend: {
+            height: {
+                "screen-85": "85vh",
+                "screen-75": "75vh",
+                "screen-50": "50vh",
+                "screen/3": "calc(100vh / 3)",
+                "screen/4": "calc(100vh / 4)",
+                "screen/5": "calc(100vh / 5)",
+            },
             colors: {
                 background: "#232325",
             },
