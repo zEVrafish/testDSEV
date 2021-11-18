ddfb5d52212ac0404559db2f25eace7590f12b6d
Anders Askeland <anders.askeland@gmail.com>
2021-11-18 12:13:50 +0100

    Prototype carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
     darkMode: "class", // or 'media' or 'class'
     theme: {
         extend: {
+            width: {
+                "carousel": "calc(50% - 4px)",
+            },
             height: {
                 "100": "28rem",
                 "104": "32rem",
